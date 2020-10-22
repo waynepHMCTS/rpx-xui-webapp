a3cdd1b9083de57682dbaa179f1d6eacaa46bf63
Lam <daniel.2.lam@atos.net>
2020-10-22 12:00:00 +0100

    Reimplement toolbar responsive behaviour
    Now uses two sets of identical buttons for the toolbar and "More options" dropdown list, and toggles styles to show/hide pairs of buttons as triggered by break-widths being hit or more screen space becoming available.

--- src/app/components/media-viewer-toolbar/media-viewer-toolbar.component.ts
+++ src/app/components/media-viewer-toolbar/media-viewer-toolbar.component.ts
@@ -16,7 +16,6 @@ export interface ButtonState {
 
 export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('zoomSelect') public zoomSelect: ElementRef;
-  // @ViewChild('dropdownMenu') public dropdownMenu: ElementRef; // Could we add and remove child nodes to the dropdown this way?
   @ViewChild('mvToolbarMain') public mvToolbarMain: ElementRef<HTMLElement>;
 
   public pageNumber = 1;
@@ -35,13 +34,8 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
       0,
       3)
   ];
-  public hiddenDropdownMenu: Element[] = [];
-  public hideDropdownMenuButton = true;
   private readonly breakWidths: number[] = [];
   private readonly toolbarButtonVisibilityStates: ButtonState[] = [];
-
-  // Reference to the main toolbar element
-  // private mvToolbarMain: HTMLElement;
 
   public constructor(
     public readonly toolbarEvents: ToolbarEventService,
@@ -66,13 +60,22 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
   public ngAfterViewInit() {
     let totalSpace = 0;
 
-    // Loop over all elements and set sum of widths for each toolbar element
-    for (const element of Array.prototype.slice.call(this.mvToolbarMain.nativeElement.children)) {
-      totalSpace += element.clientWidth;
-      this.breakWidths.push(totalSpace);
+    // Get all toolbar elements
+    const toolbarElements: HTMLElement[] = Array.prototype.slice.call(this.mvToolbarMain.nativeElement.children);
 
-      // Set a flag for this element's visibility and push to array of button states
+    // Set a flag for each element's visibility and push to array of button states
+    for (const element of toolbarElements) {
       this.toolbarButtonVisibilityStates.push({ id: element.id, showOnToolbar: true } as ButtonState);
+    }
+
+    // Trigger change detection (necessary to ensure all toolbar elements are rendered fully - and have a size - at this stage)
+    this.cdr.detectChanges();
+
+    // Loop over all elements and set sum of widths for each toolbar element
+    for (const element of toolbarElements) {
+      // Use bounding client rectangle width for accuracy
+      totalSpace += element.getBoundingClientRect().width;
+      this.breakWidths.push(totalSpace);
     }
 
     // Call calculation method
@@ -80,51 +83,42 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
   }
 
   @HostListener('window:resize', ['$event'])
-  public onResize(event: Event) {
+  public onResize() {
     // Call calculation method
     this.checkCalculation();
   }
 
   private checkCalculation() {
-    // Get current space of main toolbar element
-    const availableSpace = this.mvToolbarMain.nativeElement.offsetWidth;
+    // Get current space of main toolbar element, including the right margin (if any) to the "More options" button
+    const availableSpace = this.mvToolbarMain.nativeElement.getBoundingClientRect().width +
+    parseFloat(getComputedStyle(this.mvToolbarMain.nativeElement).marginRight);
 
     // Get space required for all elements in main toolbar element (i.e. break-width of the last visible element from breakWidth array)
-    const indexOfLastVisibleElement = Array.from(this.mvToolbarMain.nativeElement.children).length - 1;
+    const indexOfLastVisibleElement = this.getIndexOfLastVisibleToolbarButton();
     const totalSpaceRequired = this.breakWidths[indexOfLastVisibleElement];
 
     // If more space is needed than is available, hide last element
     if (totalSpaceRequired > availableSpace) {
-      // Remove the last element from the main toolbar...
-      const lastToolbarElement = this.mvToolbarMain.nativeElement.removeChild(this.mvToolbarMain.nativeElement.lastElementChild);
-
-      // ...and add it to the start of hiddenDropdownMenu
-      // this.dropdownMenu.nativeElement.appendChild(this.mvToolbarMain.lastElementChild);
-      this.hiddenDropdownMenu.unshift(lastToolbarElement);
-
-      // Show the button for the dropdown menu
-      this.hideDropdownMenuButton = false;
+      // Mark the toolbar button as hidden in the button states array
+      this.setToolbarButtonVisible(this.toolbarButtonVisibilityStates[indexOfLastVisibleElement].id, false);
 
       // Apply changes
       this.cdr.detectChanges();
 
       // Call this method to recalculate for rest of toolbar elements
       this.checkCalculation();
-    } else {
-      // If there are elements in hidden dropdown menu
-      if (this.hiddenDropdownMenu.length > 0) {
-        // Remove the first element from hiddenDropdownMenu...
-        const firstHiddenElement = this.hiddenDropdownMenu.shift();
-
-        // ...and add it to the main toolbar
-        this.mvToolbarMain.nativeElement.appendChild(firstHiddenElement);
-
-        // Hide or show the button for the dropdown menu if there no elements or some remaining respectively
-        this.hideDropdownMenuButton = !(this.hiddenDropdownMenu.length > 0);
+    } else if (this.toolbarHasHiddenButtons() && this.breakWidths[indexOfLastVisibleElement + 1] <= availableSpace) {
+      // If there are some toolbar buttons marked as hidden in the button states array, AND there is room for the first of these,
+      // mark the button as visible
+      for (const buttonState of this.toolbarButtonVisibilityStates) {
+        if (!buttonState.showOnToolbar) {
+          buttonState.showOnToolbar = true;
+          break;
+        }
       }
 
-      // Apply changes
-      this.cdr.detectChanges();
+      // Call this method to recalculate for rest of toolbar elements
+      this.checkCalculation();
     }
   }
 
@@ -136,10 +130,22 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
     }
   }
 
-  public setToolbarButtonVisible(buttonId: string, visible: boolean) {
+  private setToolbarButtonVisible(buttonId: string, visible: boolean) {
     for (const buttonState of this.toolbarButtonVisibilityStates) {
       if (buttonState.id === buttonId) {
         buttonState.showOnToolbar = visible;
+      }
+    }
+  }
+
+  public toolbarHasHiddenButtons(): boolean {
+    return this.toolbarButtonVisibilityStates.some(buttonState => buttonState.showOnToolbar === false);
+  }
+
+  private getIndexOfLastVisibleToolbarButton(): number {
+    for (let i = this.toolbarButtonVisibilityStates.length - 1; i >= 0; i--) {
+      if (this.toolbarButtonVisibilityStates[i].showOnToolbar) {
+        return i;
       }
     }
   }
@@ -164,15 +170,15 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
   }
 
   public increasePageNumber() {
-    this.toolbarEvents.changePageByDeltaSubject.next(1);
+    this.toolbarEvents.incrementPage(1);
   }
 
   public decreasePageNumber() {
-    this.toolbarEvents.changePageByDeltaSubject.next(-1);
+    this.toolbarEvents.incrementPage(-1);
   }
 
   public onPageNumberInputChange(pageNumber: string) {
-    this.toolbarEvents.setCurrentPageSubject.next(Number.parseInt(pageNumber, 0));
+    this.toolbarEvents.setPage(Number.parseInt(pageNumber, 0));
   }
 
   private setCurrentPage(pageNumber: number) {
@@ -198,5 +204,23 @@ export class MediaViewerToolbarComponent implements OnInit, OnDestroy, AfterView
   public stepZoom(zoomFactor: number) {
     this.toolbarEvents.stepZoom(zoomFactor);
     this.zoomSelect.nativeElement.selected = 'selected';
+  }
+
+  public toggleCommentsPanel() {
+    // Not available in media-viewer Version 2.3.10 but currently unable to upgrade due to build failure from Version 2.3.11 onwards
+    // (see change made in media-viewer package.json for Version 2.3.11, regarding ngx-chips)
+    // this.toolbarEvents.toggleCommentsPanel(!this.toolbarEvents.commentsPanelVisible.getValue());
+  }
+
+  public togglePresentBar() {
+    // TODO
+  }
+
+  public toggleRedactBar() {
+    this.toolbarEvents.toggleRedactionMode();
+  }
+
+  public toggleGrabNDrag() {
+    this.toolbarEvents.toggleGrabNDrag();
   }
 }
